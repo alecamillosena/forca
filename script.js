4ae02758dbfad4e6cec1402d44f86272f22caa33
unknown <alecamillosena@gmail.com>
2019-02-18 22:00:56 -0300

    Jogo da forca terminar o JS

---
+++ script.js
@@ -0,0 +1,18 @@
+const palavra = ['H', 'A', 'C', 'K']
+let letraDigitada
+
+function valorDoInput(e) {
+    let letraDigitada = document.getElementById('text').value;
+
+}
+console.log(letraDigitada());
+//para o usuario digitar uma letra
+
+n = 0;
+for n in palavra: (
+    
+    letraDigitada = n
+        
+    n++
+    )
+   
